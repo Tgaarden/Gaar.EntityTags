7c2a403fcdff9ec8baa4b9c28241b2767489110d
Thomas B Gaarden <thomas.gaarden@oxx.no>
2022-01-02 13:09:45 +0100

    Initial commit

---
+++ Gaar.EntityTags/EntityTagAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace Gaar.EntityTag
+{
+    public class EntityTagAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string etagValue = EntityTagHelper.GetEtagValueInCache();
+            var responseEtag = filterContext.HttpContext.Request.Headers["if-none-match"];
+            if (string.IsNullOrWhiteSpace(responseEtag) || responseEtag != etagValue)
+            {
+                filterContext.HttpContext.Response.Headers["etag"] = etagValue;
+
+                base.OnActionExecuting(filterContext);
+            }
+            else
+            {
+                filterContext.Result = new HttpStatusCodeResult(304);
+            }
+        }
+    }
+}
---
+++ Gaar.EntityTags/EntityTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace Gaar.EntityTag
+{
+    public static class EntityTagHelper
+    {
+        private const string EtagCacheKey = "entityTagCacheKey";
+
+        public static void UpdateEtagValueInCache(string spesificEtagCacheKey = null)
+        {
+            var cacheKey = !string.IsNullOrWhiteSpace(spesificEtagCacheKey) ? spesificEtagCacheKey : EtagCacheKey;
+            var etagValue = DateTime.UtcNow.Ticks.ToString() + ":0";
+            ObjectCache cache = MemoryCache.Default;
+            CacheItemPolicy policy = new CacheItemPolicy();
+            policy.AbsoluteExpiration = DateTimeOffset.Now.AddYears(1);
+            cache.Set(cacheKey, etagValue, policy);
+        }
+
+        public static string GetEtagValueInCache(string spesificEtagCacheKey = null)
+        {
+            var cacheKey = !string.IsNullOrWhiteSpace(spesificEtagCacheKey) ? spesificEtagCacheKey : EtagCacheKey;
+            ObjectCache cache = MemoryCache.Default;
+            string etagValue = (string)cache.Get(cacheKey);
+
+            if (string.IsNullOrWhiteSpace(etagValue))
+            {
+                UpdateEtagValueInCache();
+                return GetEtagValueInCache();
+            }
+
+            return etagValue;
+        }
+    }
+}
